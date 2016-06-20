e65bc9115b9a62e9eb384902153d8f76c9eac398
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-21 01:20:11 +0400

    v empty input
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # metabay
+### 60's metacompiler sample for ALCOGOLic language
+
 http://www.bayfronttechnologies.com/mc_tutorial.html
+
---
+++ hpp.hpp
@@ -0,0 +1,14 @@
+#ifndef _H_HPP
+#define _H_HPP
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+extern int yyparse();
+extern void yyerror(string);
+
+#endif // _H_HPP
