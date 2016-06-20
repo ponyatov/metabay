e0e49dc9da18dc98cce91aa9090a8b7462a2a1cc
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-21 01:40:11 +0400

    + html
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,5 +1,10 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() { return yyparse(); }
+int main(int argc, char *argv[]) {
+	cout << "<html>\n<script>\nconsole.log('"<<argv[0]<<"');\n";
+	yyparse();
+	cout << "</script>\n</html>\n";
+	return 0;
+}
 
